095cb9feaf3a52eb22c9bf5e95db5293763f6eda
Wiznikvibe <nikhilshetty439@gmail.com>
2025-07-12 18:18:20 +0530

    Add MLflow integration for ElasticNet model training and logging

---
+++ MLFLOW_AWS_CLOUD/mlflow-aws/app.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+if __name__ == "__main__":
+    csv_url = (
+        "https://raw.githubusercontent.com/mlflow/mlflow/master/tests/datasets/winequality-red.csv"
+    )
+    
+    try:
+        data = pd.read_csv(csv_url, sep=";")
+    except Exception as e:
+        logger.exception("Unable to download the data")
+        sys.exit(1)
+    
+    train, test = train_test_split(data)
+    
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+    
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    
+    # Set tracking URI BEFORE starting the run
+    remote_server_uri = "http://ec2-3-89-232-177.compute-1.amazonaws.com:5000/"
+    mlflow.set_tracking_uri(remote_server_uri)
+    
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+        
+        predicted_output = lr.predict(test_x)
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_output)
+        
+        print("RMSE: %s" % rmse)
+        print("MAE: %s" % mae)
+        print("R2: %s" % r2)
+        
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2_score", r2)
+        
+        # Create signature for the model
+        signature = infer_signature(train_x, lr.predict(train_x))
+        
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(
+                lr, 
+                "model", 
+                registered_model_name="ElasticNetWineModel",
+                signature=signature,
+                input_example=train_x.iloc[:5]
+            )
+        else:
+            mlflow.sklearn.log_model(lr, "model", signature=signature)
