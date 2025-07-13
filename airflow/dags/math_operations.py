455f1432042ae0af16a48b817b7ac172b7f8cdcd
Wiznikvibe <nikhilshetty439@gmail.com>
2025-07-13 18:28:08 +0530

    Add initial DAG definition for math operations

---
+++ airflow/dags/math_operations.py
@@ -0,0 +1,9 @@
+"""
+We'll define a DAG where the tasks are as follows:
+
+TASK 1: Start with an initial number (example: 10)
+TASK 2: Add 5 to the number 
+TASK 3: Multiply the result with 2 
+TASK 4: Subtract 3 from the result 
+TASK 5: Compute the square of the result 
+"""
